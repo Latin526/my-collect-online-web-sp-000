6739f30fc77e6f13eb5dfc6da3ac51406547eae9
Yasmin Torres <yasmintorres90@hotmail.com>
2018-11-06 23:39:42 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,9 @@
- def my_collect(arr)
-  arr = ["STAR WARS", "STAR TREK", "WONDER WOMAN"]
-   my_collect(arr) do |name|
-    name.spilt(" ").lowercase
+def my_collect(collection)
+  i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+  end
+  new_collection
 end
